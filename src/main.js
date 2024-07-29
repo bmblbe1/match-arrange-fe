85e941ee3fe810361abd337db284732527f7eb4d
bmblbe1 <paolo.peloza@gmail.com>
2024-07-29 20:10:13 +0200

    Login Page , On login click to do, register page to do

--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
+import { createWebHistory, createRouter } from 'vue-router'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import LoginPage from './pages/login/LoginPage.vue';
+import RegisterPage from './pages/register/RegisterPage.vue';
+
+
+const routes = [
+  { path: '/', component: LoginPage },
+  { path: '/login', component: LoginPage },
+  { path: '/register', component: RegisterPage },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+})
+
+createApp(App)
+  .use(router)
+  .mount('#app')
---
+++ src/pages/login/LoginPage.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="container">
+        <div class="box">
+            <h1>
+                Login to Page
+            </h1>
+            <input type="email" placeholder="Enter your Email" class="input-field"/>
+            <input type="password" placeholder="Enter your Password" class="input-field"/>
+            <button @click="logMessage">Login</button>
+            <div>
+                Do not have an account?  <router-link to="/register">Register here.</router-link>
+            </div>
+        </div>
+    </div>
+</template>
+<style scoped>
+    .container {
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+ 
+    .box {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        border: 2px solid #021526;
+        border-radius: 12px;
+        padding: 24px;
+        gap: 16px;
+
+        h1 {
+            text-align: center;
+        }
+    }
+
+    .input-field {
+        padding: 12px;
+        border: 2px solid #021526;
+        border-radius: 4px;
+    }
+    button{
+        font-weight: bold;
+        padding: 4px;
+    } 
+</style>
+<script>
+    export default {
+    name: 'LoginPage',
+    methods: {
+    logMessage() {
+      console.log('Button clicked');
+    }
+  }
+}
+</script>
